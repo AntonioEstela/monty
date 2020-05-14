9f2baa6b3693f47df6cc71cf1f140431fb138c90
Antonio Estela <antonioestela73@gmail.com>
2020-05-14 19:25:37 +0000

    Added some functions

--- _rotl.c
+++ _rotl.c
@@ -6,8 +6,12 @@
 void _rotl(stack_t **h, unsigned int times)
 {
 	stack_t *list = *h;
-	int tmp = list->n;
+	int tmp;
 
+	if (*h == NULL || h == NULL)
+		exit(EXIT_FAILURE);
+	
+	tmp = list->n;
 	add_dnodeint_end(h, tmp);
 	_pop(h, times);
 }
