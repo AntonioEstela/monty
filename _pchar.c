af1fb6fe90612a86c4cfa68dcb9329c33fe55236
Antonio Estela <antonioestela73@gmail.com>
2020-05-14 18:06:46 +0000

    Fixed some things

--- _pchar.c
+++ _pchar.c
@@ -5,15 +5,15 @@
  */
 void _pchar(stack_t **h, unsigned int times)
 {
-	if ((*h)->n > 127 || (*h)->n < 0)
-	{
-		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", times);
-		_free(*h);
-		exit(EXIT_FAILURE);
-	}
 	if (*h == NULL || h == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", times);
+		_free(*h);
+		exit(EXIT_FAILURE);
+	}
+	if ((*h)->n > 127 || (*h)->n < 0)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", times);
 		_free(*h);
 		exit(EXIT_FAILURE);
 	}
